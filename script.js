678bde9bf61e3a4c62f27f44698f0d1742e4faed
iluvdsh <serikovardak0@gmail.com>
2026-01-21 00:42:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
 const slides = document.querySelectorAll('.slide');
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.prev');
-
 let index = 0;
 
-function showSlide(i) {
+function show(i) {
   slides[index].classList.remove('active');
   index = (i + slides.length) % slides.length;
   slides[index].classList.add('active');
 }
 
-nextBtn.addEventListener('click', () => {
-  showSlide(index + 1);
-});
-
-prevBtn.addEventListener('click', () => {
-  showSlide(index - 1);
-});
-
-/* свайп для телефона */
-let startX = 0;
-
-document.addEventListener('touchstart', e => {
-  startX = e.touches[0].clientX;
-});
-
-document.addEventListener('touchend', e => {
-  const diff = startX - e.changedTouches[0].clientX;
-  if (Math.abs(diff) > 50) {
-    diff > 0 ? showSlide(index + 1) : showSlide(index - 1);
-  }
-});
+document.querySelector('.next').onclick = () => show(index + 1);
+document.querySelector('.prev').onclick = () => show(index - 1);
