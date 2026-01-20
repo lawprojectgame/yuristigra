df371dbbb6590b46162a20c0ee26ec62cf7aec77
iluvdsh <serikovardak0@gmail.com>
2026-01-21 00:54:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
 let index = 0;
 
-function show(i) {
+function showSlide(i) {
   slides[index].classList.remove('active');
   index = (i + slides.length) % slides.length;
   slides[index].classList.add('active');
 }
 
-document.querySelector('.next').onclick = () => show(index + 1);
-document.querySelector('.prev').onclick = () => show(index - 1);
+nextBtn.onclick = () => showSlide(index + 1);
+prevBtn.onclick = () => showSlide(index - 1);
+
+/* ===== SWIPE FOR MOBILE ===== */
+
+let startX = 0;
+
+document.addEventListener('touchstart', e => {
+  startX = e.touches[0].clientX;
+});
+
+document.addEventListener('touchend', e => {
+  const diff = startX - e.changedTouches[0].clientX;
+  if (Math.abs(diff) > 50) {
+    diff > 0 ? showSlide(index + 1) : showSlide(index - 1);
+  }
+});
